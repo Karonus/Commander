67aaeea89d051f129c1708f1f27d4560cf761d9c
Karonus <46893740+Karonus@users.noreply.github.com>
2020-05-26 22:29:13 +0300

    Add files via upload

---
+++ karonus/lang/ru_RU.java
@@ -0,0 +1,17 @@
+package karonus.lang;
+
+public class ru_RU {
+	private static String error = "§cОшибка!";
+	private static String errorPerm = "§cОшибка! У вас недостаточно прав для выполнения данной команды §c(§fcommander.cmd§c)§c.";
+	private static String success = "§aУспешно!";
+	
+	public static String getErrorMes() {
+		return error;
+	}
+	public static String getErrorPermMes() {
+		return errorPerm;
+	}
+	public static String getSuccessMes() {
+		return success;
+	}
+}
