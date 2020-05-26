39efa2c9aa6ba3bc8f8231df36754c3b73c2a5ca
Karonus <46893740+Karonus@users.noreply.github.com>
2020-05-26 22:28:57 +0300

    Create en_EN.java

---
+++ karonus/lang/en_EN.java
@@ -0,0 +1,17 @@
+package karonus.lang;
+
+public class en_EN {
+	private static String error = "§cError!";
+	private static String errorPerm = "§cError! You don't have permissions §c(§fcommander.cmd§c)§c.";
+	private static String success = "§aSuccess!";
+	
+	public static String getErrorMes() {
+		return error;
+	}
+	public static String getErrorPermMes() {
+		return errorPerm;
+	}
+	public static String getSuccessMes() {
+		return success;
+	}
+}
